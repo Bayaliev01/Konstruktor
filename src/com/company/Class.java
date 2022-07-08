8064d8207f55a6c4ff10da6e5f8573ff58bc01b8
lesson1 <bayaliev07@gmail.com>
2022-07-08 17:32:35 +0300

    salsm

--- src/com/company/Class.java
+++ src/com/company/Class.java
@@ -1,2 +1,42 @@
-package com.company;public class Class {
+package com.company;
+
+import java.util.Arrays;
+
+public class Class {
+    private int nomer;
+    private String soz;
+    private int[] san;
+
+    Class(int nomer, String soz, int[] san) {
+        this.nomer = nomer;
+        this.soz = soz;
+        this.san = san;
+
+    }
+
+
+    public int getNomer() {
+        return nomer;
+    }
+
+    public void setNomer(int nomer) {
+        this.nomer = nomer;
+    }
+
+    public String getSoz() {
+        return soz;
+    }
+
+    public void setSoz(String soz) {
+        this.soz = soz;
+    }
+
+    public String getSan() {
+        return Arrays.toString(san);
+
+    }
+
+    public void setSan(int[] san) {
+        this.san = san;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,16 @@
+package com.company;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Class class1 = new Class(99, "hello world", new int[]{1, 5, 9, 6, 4});
+        Class class2 = new Class(33, "salam", new int[]{30, 40, 50, 60});
+
+        System.out.println(class1.getNomer());
+        System.out.println(class1.getSoz());
+        System.out.println(class1.getSan());
+        System.out.println(class2.getNomer());
+        System.out.println(class2.getSoz());
+        System.out.println(class2.getSan());
+
     }
 }
